b0762f64cda108a6a5b22db944f12b5eeef7868b
Khaled85abo <khaledabou1985@gmail.com>
2022-01-24 18:40:34 +0100

    Form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {
+  StyledCard,
+  FlexCol,
+  Flex,
+  ChipLogo,
+  Span,
+  Form,
+  Input,
+  Submit,
+  Label,
+  Select,
+} from "./styledComponents/StyledComponents";
+import { Chip, Wifi, Bitcoin, BlockChain, Ninja, Evil } from "./assets";
+import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StyledCard>
+        <ChipLogo>
+          <FlexCol>
+            <Wifi />
+            <Chip />
+          </FlexCol>
+          <Evil />
+        </ChipLogo>
+        <Flex>
+          <Span>XXXX</Span>
+          <Span>XXXX</Span>
+          <Span>XXXX</Span>
+          <Span>XXXX</Span>
+        </Flex>
+        <Flex>
+          <FlexCol>
+            <h4>name</h4>
+            <h4>Card holder name</h4>
+          </FlexCol>
+          <FlexCol>
+            <h4>Validality</h4>
+            <h4>MM/YY</h4>
+          </FlexCol>
+        </Flex>
+      </StyledCard>
+      <Form>
+        <Label>Card Number</Label>
+        <Input
+          placeholder="Enter card number"
+          id="card-number"
+          type="number"
+          min="16"
+        />
+        <Label>Card Owner</Label>
+        <Input type="text" id="owner" placeholder="Full name" />
+        <Submit type="submit" />
+      </Form>
     </div>
   );
 }
---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import BlockChain from "./BlockChain";
+import Chip from "./Chip";
+import Wifi from "./Wifi";
+import Bitcoin from "./Bitcoin";
+import Evil from "./Evil";
+import Ninja from "./Ninja";
+
+export { BlockChain, Chip, Wifi, Bitcoin, Evil, Ninja };
